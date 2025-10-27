0a06b392249c000fdb02476cd55d535feb3fbb52
art14go <artgonon4@gmail.com>
2025-10-27 09:41:00 +0100

    use user input to display exercise specifics

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import duckdb
+import ast
 
 con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
 
@@ -15,39 +16,41 @@ with st.sidebar:
 
     st.write("You selected", theme)
 
-    exercise = con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}'").df()
+    exercise = con.execute(f"SELECT * FROM memory_state_df WHERE theme = '{theme}'").df()
     st.write(exercise)
 
 st.header("Enter your code:")
 
 query = st.text_area(label="Your SQL code here", key="user_input")
 
-# if query:
-#     result = duckdb.sql(query).df()
-#     st.dataframe(result)
+if query:
+    result = con.execute(query).df()
+    st.dataframe(result)
+
+#    try:
+#        result = result[solution_df.columns]
+#    except KeyError as e:
+#        st.write("Some columns are missing")
 #
-#     try:
-#         result = result[solution_df.columns]
-#     except KeyError as e:
-#         st.write("Some columns are missing")
+#    n_lines_difference = result.shape[0] - solution_df.shape[0]
+#    if n_lines_difference != 0:
+#        st.write(
+#            f"result has a {n_lines_difference} lines difference with the solution_df"
+#        )
 #
-#     n_lines_difference = result.shape[0] - solution_df.shape[0]
-#     if n_lines_difference != 0:
-#         st.write(
-#             f"result has a {n_lines_difference} lines difference with the solution_df"
-#         )
+#    st.dataframe(result.compare(solution_df))
 #
-#     st.dataframe(result.compare(solution_df))
-#
-# tab2, tab3 = st.tabs(["Tables", "Solution"])
-#
-# with tab2:
-#     st.write("table: beverages")
-#     st.dataframe(beverages)
-#     st.write("table: food_items")
-#     st.dataframe(food_items)
-#     st.write("expected:")
-#     st.dataframe(solution)
-#
-# with tab3:
-#     st.write(answer)
+tab2, tab3 = st.tabs(["Tables", "Solution"])
+
+with tab2:
+    exercise_tables = ast.literal_eval(exercise.loc[0, "tables"])
+    for table in exercise_tables:
+        st.write(f"table: {table}")
+        df_table = con.execute(f"SELECT * FROM {table}").df()
+        st.dataframe(df_table)
+
+with tab3:
+    exercise_name = exercise.loc[0, "exercise_name"]
+    with open(f"answer/{exercise_name}", "r") as f:
+        answer = f.read()
+    st.write(answer)
