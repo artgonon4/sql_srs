72918ec3143c2e993521bbba8795ed9da5906257
art14go <artgonon4@gmail.com>
2025-10-27 09:52:21 +0100

    comparison with real answers

--- app.py
+++ app.py
@@ -19,6 +19,12 @@ with st.sidebar:
     exercise = con.execute(f"SELECT * FROM memory_state_df WHERE theme = '{theme}'").df()
     st.write(exercise)
 
+    exercise_name = exercise.loc[0, "exercise_name"]
+    with open(f"answer/{exercise_name}", "r") as f:
+        answer = f.read()
+
+    solution_df = con.execute(answer).df()
+
 st.header("Enter your code:")
 
 query = st.text_area(label="Your SQL code here", key="user_input")
@@ -27,19 +33,18 @@ if query:
     result = con.execute(query).df()
     st.dataframe(result)
 
-#    try:
-#        result = result[solution_df.columns]
-#    except KeyError as e:
-#        st.write("Some columns are missing")
-#
-#    n_lines_difference = result.shape[0] - solution_df.shape[0]
-#    if n_lines_difference != 0:
-#        st.write(
-#            f"result has a {n_lines_difference} lines difference with the solution_df"
-#        )
-#
-#    st.dataframe(result.compare(solution_df))
-#
+    try:
+        result = result[solution_df.columns]
+        st.dataframe(result.compare(solution_df))
+    except KeyError as e:
+        st.write("Some columns are missing")
+
+    n_lines_difference = result.shape[0] - solution_df.shape[0]
+    if n_lines_difference != 0:
+        st.write(
+            f"result has a {n_lines_difference} lines difference with the solution_df"
+        )
+
 tab2, tab3 = st.tabs(["Tables", "Solution"])
 
 with tab2:
